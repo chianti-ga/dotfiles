9d126804b59e991540cd882e2aea5acba67c07a8
skitou <chiantigally@gmail.com>
2022-05-15 04:20:54 +0200

    fix

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-cp .zshrc ~
-cp .p10k.zsh ~
+yes | cp .zshrc ~
+yes | cp .p10k.zsh ~
