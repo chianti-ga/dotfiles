62576ae5fde82630778bd3288109e7a1da2bb0ea
skitou <chiantigally@gmail.com>
2022-05-15 12:26:09 +0200

    fix files not coppying

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ elif [[ "$1" = "arch" ]]
 then sudo pacman -Syu curl zsh
 fi
 
+echo "Isntalling.."
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-yes | cp -rf .zshrc ~
-yes | cp -rf .p10k.zsh ~
+
+rm ~/.zshrc; cp .zshrc ~
+rm ~/.p10k.zsh; cp .p10k.zsh ~
--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+echo "Isntalling.."
+
 cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/
 rm -rf powerlevel10k
 cd ~
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-yes | cp -rf .zshrc ~
-yes | cp -rf .p10k.zsh ~
+rm ~/.zshrc; cp .zshrc ~
+rm ~/.p10k.zsh; cp .p10k.zsh ~
