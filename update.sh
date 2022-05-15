770d7286ceca0fabb267041f8b8495483fe91d66
skitou <chiantigally@gmail.com>
2022-05-15 04:01:25 +0200

    fix

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/
+rm -rf powerlevel10k
+cd ~
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp .zshrc ~
 cp .p10k.zsh ~
