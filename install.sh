c4160791fa5816bc1f5563d93e0c9d160940fcd6
skitou <chiantigally@gmail.com>
2022-05-15 03:23:48 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-function install {
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    cp .zshrc ~
-    chsh -s $(which zsh)
-    typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
-}
-
-install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cp .zshrc ~
+chsh -s $(which zsh)
+typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
