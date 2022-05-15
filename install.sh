25053a67c7c53425298fb182fdc0cfb1e0d2f424
skitou <chiantigally@gmail.com>
2022-05-15 03:58:51 +0200

    working

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [[ "$1" = "debian" || "$1" = "ubuntu" ]]
+then sudo bash -c "apt update -y && apt install -y  curl zsh"
+elif [[ "$1" = "arch" ]]
+then sudo pacman -Syu curl zsh
+fi
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp .zshrc ~
-typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
+cp .p10k.zsh ~
--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp .zshrc ~
-typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
+cp .p10k.zsh ~
