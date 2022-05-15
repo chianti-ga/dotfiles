5f9e4cdc1db39dfc4b709f5966eee974e99d332d
skitou <chiantigally@gmail.com>
2022-05-15 12:31:28 +0200

    fix files not coppying

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 echo "Isntalling.."
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd ~/zsh-autoconfig
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 rm ~/.zshrc; cp .zshrc ~
 rm ~/.p10k.zsh; cp .p10k.zsh ~
