fca85ef98b406bdc35c6fbd844f03340b6e987d9
skitou <chiantigally@gmail.com>
2022-08-18 02:22:04 +0200

    Updated dir name

--- install.sh
+++ install.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-if [[ "$1" = "debian" || "$1" = "ubuntu" ]]
-then sudo bash -c "apt update -y && apt install -y  curl zsh"
-elif [[ "$1" = "arch" ]]
-then sudo pacman -Syu curl zsh
-fi
-
 echo "Isntalling.."
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cd ~/zsh-autoconfig
+cd ~/dotfiles
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm ~/.zshrc; cp .zshrc ~
 rm ~/.p10k.zsh; cp .p10k.zsh ~
--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ echo "Isntalling.."
 
 cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/
 rm -rf powerlevel10k
-cd ~/zsh-autoconfig
+cd ~/dotfiles
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm ~/.zshrc; cp .zshrc ~
 rm ~/.p10k.zsh; cp .p10k.zsh ~
