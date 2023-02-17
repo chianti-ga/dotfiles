c3bb69d2380036caa8ea37abe72873544105d500
skitou <chiantigally@gmail.com>
2023-02-17 22:03:51 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,3 +8,4 @@ cd ~/dotfiles
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm ~/.zshrc; cp .zshrc ~
 rm ~/.p10k.zsh; cp .p10k.zsh ~
+rm ~/.tmux.conf; cp .tmux.conf ~
