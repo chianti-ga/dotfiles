da6f3275e6998d7ff791502293d0130fc7966167
skitou <chiantigally@gmail.com>
2023-02-17 22:10:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ cd ~/dotfiles
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm ~/.zshrc; cp .zshrc ~
 rm ~/.p10k.zsh; cp .p10k.zsh ~
+rm ~/.tmux.conf; cp .tmux.conf ~
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
